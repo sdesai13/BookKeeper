f523153145626af339ae738d49d32f8295f8cfeb
Shwetang Desai <96672435+sdesai13@users.noreply.github.com>
2024-09-07 19:25:30 -0400

    initialize

---
+++ server/index.js
@@ -0,0 +1,53 @@
+require("dotenv").config();
+
+const dbConnect = require("./connectDB");
+
+const cors = require("cors");
+
+const express = require("express");
+
+const Book = require("./models/Book");
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+dbConnect();
+// middle communications
+
+app.use(cors());
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+
+
+
+
+
+app.get("/", (req,res) => {
+    res.json("Hello there!");
+})
+
+app.get("/api/books", async(req,res) => {
+    try {
+       const data = await Book.find({});
+       res.json(data);
+       
+        
+    } catch (error) {
+        res.status(500).json({error: "AN ERROR OCCURED WHILE FETCHING DATA."})
+        
+    }
+})
+
+
+
+app.get("*", (req,res) => {
+    res.sendStatus("404");
+})
+
+app.listen(PORT, () => {
+    console.log(`servr is running on PORT: ${PORT}`)
+})
+
+
