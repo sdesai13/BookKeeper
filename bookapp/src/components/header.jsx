46eeeb9432440a548f22c71b95aed08b368ff5de
Shwetang Desai <96672435+sdesai13@users.noreply.github.com>
2024-12-10 14:28:33 -0500

    minor changes

--- bookapp/src/components/header.jsx
+++ bookapp/src/components/header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
-import logo from "../../public/openbook.png";
+import logo from "/openbook.png";
 
 const Header = () => {
   return (
